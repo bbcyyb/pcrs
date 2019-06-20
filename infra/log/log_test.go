fec8293b08ad5cefbdaace8b6037b991d445ae62
Kevin.Y.Yu <bbcyyb@gmail.com>
2019-06-20 22:12:28 +0800

    Complete log_test.go

--- infra/log/log_test.go
+++ infra/log/log_test.go
@@ -48,11 +48,38 @@ func TestDebugJsonLog(t *testing.T) {
 				ass.Equal("debug", l.Level)
 			}
 		}
-		//assert.Equal(t, expectedObj, actualObj)
 	}
 }
 
 func TestInfoTextLog(t *testing.T) {
+	ass := assert.New(t)
+	buf := &bytes.Buffer{}
 	SetFormatter(TEXT)
 	SetLevel(InfoLevel)
+	SetOutput(buf)
+
+	Error("Errorln")
+	Warn("Warnln")
+	Infof("Infoln %s", "with f")
+	Debug("Debugln")
+	Trace("Traceln")
+
+	slice, err := refresh()
+	if ass.NoError(err) {
+		ass.Equal(3, len(slice))
+		for index, val := range slice {
+			//expected value looks like
+			//time="2019-06-20T20:45:17.928749+08:00" level=error msg=Errorln
+			var l logger
+			json.Unmarshal([]byte(val), &l)
+			switch index {
+			case 0:
+				ass.Contains(val, "level=error msg=Errorln")
+			case 1:
+				ass.Contains(val, "level=warning msg=Warnln")
+			case 2:
+				ass.Contains(val, "level=info msg=\"Infoln with f\"")
+			}
+		}
+	}
 }
